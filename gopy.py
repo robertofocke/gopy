176e90dd0702dd1fca63b353be0acaec0a16c758
Roberto Focke <robertofocke@users.noreply.github.com>
2020-08-23 15:55:03 -0300

    Add files via upload

---
+++ gopy.py
@@ -0,0 +1,12 @@
+import requests
+
+headers={"User-Agent": "Mozilla"}
+emails=input("lista de posibles usuariso: ")
+f=open(emails,"r")
+for linea in f:
+	respuesta=requests.get("https://mail.google.com/mail/gxlu?email="+linea[:-1],headers=headers)
+	
+	if 'Set-Cookie' in respuesta.headers:
+		print (linea[:-1])
+f.closed
+	
